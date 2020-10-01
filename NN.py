2ce7043d795a63a81319caeb675e0e81da2cf24d
Erland Grimstad <44782940+erlandg@users.noreply.github.com>
2020-10-01 15:01:56 +0200

    Add files via upload

--- NN.py
+++ NN.py
@@ -3,26 +3,35 @@ import matplotlib.pyplot as plt
 
 # Augmented data vectors, (1, x1, x2, cl)
 N = 100
-cov = np.array([[0.01, 0],
-                [0, 0.01]])
+var = 0.05
+cov = np.array([[var, 0],
+                [0, var]])
 m1 = np.array([0., 0.])
 m2 = np.array([1., 1.])
 m3 = np.array([0., 1.])
 m4 = np.array([1., 0.])
 
-p1 = np.hstack((np.ones([N,1]), np.random.multivariate_normal(m1, cov, size=N),
+p1 = np.hstack((np.ones([N,1]),
+                np.random.multivariate_normal(m1, cov, size=N),
                 np.zeros([N,1])))
-p2 = np.hstack((np.ones([N,1]), np.random.multivariate_normal(m2, cov, size=N),
+p2 = np.hstack((np.ones([N,1]),
+                np.random.multivariate_normal(m2, cov, size=N),
                 np.zeros([N,1])))
-p3 = np.hstack((np.ones([N,1]), np.random.multivariate_normal(m3, cov, size=N),
+p3 = np.hstack((np.ones([N,1]),
+                np.random.multivariate_normal(m3, cov, size=N),
                 np.ones([N,1])))
-p4 = np.hstack((np.ones([N,1]), np.random.multivariate_normal(m4, cov, size=N),
+p4 = np.hstack((np.ones([N,1]),
+                np.random.multivariate_normal(m4, cov, size=N),
                 np.ones([N,1])))
+
+plt.subplot(121)
+plt.plot(p1[:,1], p1[:,2], 'r.')
+plt.plot(p2[:,1], p2[:,2], 'r.')
+plt.plot(p3[:,1], p3[:,2], 'b.')
+plt.plot(p4[:,1], p4[:,2], 'b.')
 
 data = np.vstack((p1, p2, p3, p4)).T
-
 np.random.shuffle(data.T)
-
 
 
 N_test = 50
@@ -43,7 +52,6 @@ np.random.shuffle(data.T)
 
 
 
-
 class Multilayer_Perceptron:
     """
     Creates a multilayer perceptron with any given number of neurons and layers
@@ -60,6 +68,9 @@ class Multilayer_Perceptron:
 
 
     def __reset__(self):
+        """
+        Resets results after completed epoch.
+        """
         self.v_gr = []
         self.y_gr = [self.X.copy()]
 
@@ -181,6 +192,7 @@ class Multilayer_Perceptron:
 
 
 # Design model. Here as a two-layer model with two neurons in the hidden layer
+# +1 pga augmented vectors.
 MP = Multilayer_Perceptron(data[:3, :], data[3, :])
 MP.add_layer(3)
 MP.add_layer(1)
@@ -188,10 +200,11 @@ MP.forward_computation()
 
 
 old = None
+max_epoch = 200
 epoch = 0
 tr_acc = []
 te_acc = []
-while epoch < 400:
+while epoch < max_epoch:
     epoch += 1
     old = MP.weights
     MP.backwards_propagation()
@@ -205,6 +218,7 @@ while epoch < 400:
     act_y_te = test_data[3, :].astype('int')
     te_acc.append(np.sum(est_y_te == act_y_te)/est_y_te.shape[1])
 
+
 # Accuracy matrices
 L = np.zeros((2,2))
 for n in range(data.shape[1]):
@@ -212,7 +226,6 @@ for n in range(data.shape[1]):
 
 L_te = np.zeros((2,2))
 for n in range(test_data.shape[1]):
-
     L_te[act_y_te[n], est_y_te[0,n]] += 1
 
 print(L)
@@ -221,7 +234,9 @@ print(r'Training data accuracy: %s quantile' % str(tr_acc[-1]))
 print(L_te)
 print(r'Test data accuracy: %s quantile' % str(te_acc[-1]))
 
+
 # Plot of training- and test accuracy as a function of epochs.
+plt.subplot(122)
 plt.plot(tr_acc, 'r-', label='Accuracy on training data')
 plt.plot(te_acc, 'b-', label='Accuracy on testing data')
 plt.legend()
